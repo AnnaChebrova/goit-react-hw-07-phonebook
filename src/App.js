2101a278b2a457e543fbcb48381295dc21946284
AnnaChebrova <2005viper@ukr.net>
2021-07-16 13:58:40 +0300

    add task3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import user from './components/Profile/user.json'
 import Profile from './components/Profile/User'
 import Statistics from './components/Statistics/Statistics'
 import statistics from './components/Statistics/statistical-data.json'
+import FriendList from './components/FriendList/FriendList'
+import friends from './components/FriendList/friends.json'
 
 const App = () => {
     return (
@@ -22,7 +24,14 @@ const App = () => {
         title={statistics.title}
         stats={statistics.label}
         />
+        </div>
 
+        <div>
+        <FriendList
+        name={friends.name}
+        isOnline={friends.isOnline}
+        avatar={friends.avatar}
+        />
         </div>
         </>
     );
